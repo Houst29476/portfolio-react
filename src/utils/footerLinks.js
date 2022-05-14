e11705263e238e71fa732f5b8370ffd8d0fda1b5
Bradley Boyd <bradley.boyd@gmail.com>
2022-05-14 16:20:00 -0500

    updated color palette

--- src/utils/footerLinks.js
+++ src/utils/footerLinks.js
@@ -1,37 +1,37 @@
 import {
-	faGithubSquare,
-	faHackerrank,
-	faLinkedin,
-	faStackOverflow,
-} from '@fortawesome/free-brands-svg-icons';
+  faGithubSquare,
+  faHackerrank,
+  faLinkedin,
+  faStackOverflow,
+} from "@fortawesome/free-brands-svg-icons";
 
 export const LINKS = [
-	{
-		id: 'link-1',
-		name: 'LinkedIn',
-		url: 'https://www.linkedin.com/in/bradley-boyd/',
-		icon: 'linkedin',
-		iconName: faLinkedin,
-	},
-	{
-		id: 'link-2',
-		name: 'GitHub',
-		url: 'https://github.com/houst29476',
-		icon: 'github-square',
-		iconName: faGithubSquare,
-	},
-	{
-		id: 'link-3',
-		name: 'HackerRank',
-		url: 'https://www.hackerrank.com/bradley_boyd',
-		icon: 'hackerrank',
-		iconName: faHackerrank,
-	},
-	{
-		id: 'link-4',
-		name: 'Stack Overflow',
-		url: 'https://stackoverflow.com/users/17557950/bradley-boyd',
-		icon: 'stack-overflow',
-		iconName: faStackOverflow,
-	},
+  {
+    id: "link-1",
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/bradley-boyd/",
+    icon: "linkedin",
+    iconName: faLinkedin,
+  },
+  {
+    id: "link-2",
+    name: "GitHub",
+    url: "https://github.com/houst29476",
+    icon: "github-square",
+    iconName: faGithubSquare,
+  },
+  {
+    id: "link-3",
+    name: "HackerRank",
+    url: "https://www.hackerrank.com/bradley_boyd",
+    icon: "hackerrank",
+    iconName: faHackerrank,
+  },
+  {
+    id: "link-4",
+    name: "Stack Overflow",
+    url: "https://stackoverflow.com/users/17557950/bradley-boyd",
+    icon: "stack-overflow",
+    iconName: faStackOverflow,
+  },
 ];
